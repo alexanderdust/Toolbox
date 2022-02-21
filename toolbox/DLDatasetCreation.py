630a23a9b98400a6d5e0b66b4a1331ef77dba63b
Alessandro Rinaldi <alessandro.rinaldi989@gmail.com>
2022-02-21 14:57:52 +0100

    Initial Push

---
+++ toolbox/DLDatasetCreation.py
@@ -0,0 +1,45 @@
+# Create datasets from scratch to use in DeepLearning
+
+def draw_triangle():
+    dx = np.random.uniform(0.1, 0.3)
+    dy = np.random.uniform(0.1, 0.3)
+    noise_x = np.random.uniform(0.0, 0.1)
+    noise_y = np.random.uniform(0.0, 0.1)
+
+    x = np.random.uniform(0, 1-dx-noise_x)
+    y = np.random.uniform(0, 1-dy)
+    X = np.array([[x,y], [x+dx+noise_x,y], [x+dx/2, y+dy+noise_y]])
+
+    t1 = plt.Polygon(X, color='black')
+    plt.gca().add_patch(t1)
+
+def draw_circle():
+    r = np.random.uniform(0.1, 0.25)
+    x = np.random.uniform(0+r, 1-r)
+    y = np.random.uniform(0+r, 1-r)
+
+    circle1 = plt.Circle((x, y), r, color='black')
+    plt.gcf().gca().add_artist(circle1)
+
+def create_image(form, path):
+    plt.figure(figsize=(1, 1))
+    if form == 'circle':
+        draw_circle()
+    elif form == 'triangle':
+        draw_triangle()
+    plt.axis('off')
+    plt.savefig(path, dpi=80, bbox_inches='tight')
+    plt.close()
+
+
+def create_images(path):
+    nb_circles = 100
+    nb_triangles = 100
+
+    for i in range(nb_circles):
+        c_path = os.path.join(path, 'circles', f'circle_{i}.png')
+        create_image('circle', c_path)
+
+    for i in range(nb_triangles):
+        t_path = os.path.join(path, 'triangles', f'triangle_{i}.png')
+        create_image('triangle', t_path)
